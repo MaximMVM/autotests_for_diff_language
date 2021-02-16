4438132eeea33d444e158125791772070167ed7e
MaximMVM <maksim1981m@gmail.com>
2021-02-17 02:46:29 +0300

    Some minor fix

--- test_items.py
+++ test_items.py
@@ -3,9 +3,7 @@
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-
-
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     assert browser.find_element_by_css_selector(".btn-add-to-basket")
-    #assert text == 'Войти или зарегистрироваться'
+    
