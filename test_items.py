8d6da6cfe8f550b56ac5d9337c4e0d982004182f
MaximMVM <maksim1981m@gmail.com>
2021-02-17 02:49:58 +0300

    Some minor fix

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_guest_should_see_login_link(browser):
+def test_item_has_add_to_cart_button(browser):
     browser.get(link)
     assert browser.find_element_by_css_selector(".btn-add-to-basket")
     
